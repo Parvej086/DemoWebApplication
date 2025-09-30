7d27a9703d2387bfb83693b443c4a67f0c176141
Parvej086 <your.email@example.com>
2025-09-30 21:44:37 +0530

    Initial commit: Pushing complete project

--- EmployeeApi/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EmployeeApi.Models
-{
-    public class User
-    {
-        public string Username { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-        public string Role { get; set; } = "User"; // Can be "Admin", "User", etc.
-    }
-}
